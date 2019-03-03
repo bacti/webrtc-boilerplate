531d55e793141d9ac10c8b3a42abc10ed642ba11
Hai Bacti <tranmhaii@gmail.com>
2019-03-04 05:14:54 +0700

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const Init = event =>
+{
+    try
+    {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        handleSuccess(stream)
+        event.target.disabled = true
+    }
+    catch (error)
+    {
+        handleError(error)
+    }
+  }
+
 window.main = _ =>
 {
+    document.querySelector('#showVideo').addEventListener('click', Init)
 }
