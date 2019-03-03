ad9ee836c6881290aaf9ab76c1201918f07f5e97
Hai Bacti <tranmhaii@gmail.com>
2019-03-04 05:28:10 +0700

    +

--- src/index.js
+++ src/index.js
@@ -6,26 +6,28 @@ const Constraints =
 
 function HandleSuccess(stream)
 {
-    console.log(stream)
-    const video = document.querySelector('video');
-    const videoTracks = stream.getVideoTracks();
-    console.log('Got stream with constraints:', constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
-    window.stream = stream; // make variable available to browser console
-    video.srcObject = stream;
+    const videoTracks = stream.getVideoTracks()
+    const video = document.querySelector('video')
+    video.srcObject = stream
+    console.log('Got stream with constraints:', Constraints)
+    console.log(`Using video device: ${videoTracks[0].label}`)
 }
   
 function HandleError(error)
 {
-    if (error.name === 'ConstraintNotSatisfiedError') {
-      let v = constraints.video;
-      errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-    } else if (error.name === 'PermissionDeniedError') {
-      errorMsg('Permissions have not been granted to use your camera and ' +
-        'microphone, you need to allow the page access to your devices in ' +
-        'order for the demo to work.');
+    if (error.name === 'ConstraintNotSatisfiedError')
+    {
+        let v = Constraints.video
+        console.log(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`)
     }
-    errorMsg(`getUserMedia error: ${error.name}`, error);
+    else
+    if (error.name === 'PermissionDeniedError')
+    {
+        console.log('Permissions have not been granted to use your camera and ' +
+        'microphone, you need to allow the page access to your devices in ' +
+        'order for the demo to work.')
+    }
+    console.log(`getUserMedia error: ${error.name}`, error)
 }
 
 const Init = event =>
@@ -38,8 +40,7 @@ const Init = event =>
     }
     catch (error)
     {
-        console.log(error)
-        // HandleError(error)
+        HandleError(error)
     }
   }
 
