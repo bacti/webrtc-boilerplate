d04bd705942e490056be83cf191fd1a7522f6a9d
Hai Bacti <tranmhaii@gmail.com>
2019-03-11 04:14:49 +0700

    +

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { h, render, Component } from 'preact'
 import { Trace } from './log'
+import './index.css'
 
 class PeerGambler extends Component
 {
@@ -12,7 +13,9 @@ class PeerGambler extends Component
     {
         return (
             <div id='container'>
-                <video id='localVideo' muted={true}></video>
+                <video id='localVideo' muted autoPlay>
+                    <source src='https://www.w3schools.com/html/mov_bbb.mp4' type='video/mp4' />
+                </video>
             </div>
         )
     }
