607d8e9431593cdec38ab340a00864be950d63a4
Hai Bacti <tranmhaii@gmail.com>
2019-03-10 10:23:44 +0700

    +

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import { h, render, Component } from 'preact'
 
-interface HelloProps { compiler: string; framework: string; }
-
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-class Hello extends Component<HelloProps, {}>
+class Hello extends Component<{ compiler: string; framework: string }, {}>
 {
     render()
     {
@@ -12,36 +8,9 @@ class Hello extends Component<HelloProps, {}>
     }
 }
 
-
-// interface AppProps {
-//     name: string;
-//   }
-  
-//   interface AppState {
-//     name: string;
-//   }
-  
-//   class App extends Component<any
-//   , AppState> {
-//     constructor(props: AppProps) {
-//       super(props);
-  
-//       this.state = { name: props.name };
-//     }
-//     componentDidMount() {
-//       setTimeout(() => { 
-//         var state = this.state;
-        
-//         this.setState({ name: "Preact's componentDidMount worked as expected" });
-//       }, 2000);
-//     }
-//     render(props: AppProps, state: AppState) {
-//       return <h1>props: {props.name} state: {state.name}</h1>
-//     }
-//   }
-
-declare global {
-    interface Window { main: any; }
+declare global
+{
+    interface Window { main: any }
 }
 
 window.main = () =>
