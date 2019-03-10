2b60ed6c0cce654d121031f0788138dee9f2b055
Hai Bacti <tranmhaii@gmail.com>
2019-03-11 05:56:17 +0700

    +

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import { Trace } from './log'
 import { SERVER_DOMAIN, SERVER_PORT } from '../config'
 import './index.css'
 
+const PEER_CONNECTION_CONFIG =
+{
+    'iceServers':
+    [
+        {'urls': 'stun:stun.stunprotocol.org:3478'},
+        {'urls': 'stun:stun.l.google.com:19302'},
+    ]
+}
+
 class PeerGambler extends Component
 {
     private socket = connect(`ws://${SERVER_DOMAIN}:${SERVER_PORT}/`, {transports: ['websocket']})
@@ -12,6 +21,18 @@ class PeerGambler extends Component
     {
         super()
         this.socket.on('connected', message => console.log(message))
+    }
+
+    Start(isCaller)
+    {
+        // peerConnection = new RTCPeerConnection(PeerConnectionConfig);
+        // peerConnection.onicecandidate = gotIceCandidate;
+        // peerConnection.ontrack = gotRemoteStream;
+        // peerConnection.addStream(localStream);
+      
+        // if(isCaller) {
+        //   peerConnection.createOffer().then(createdDescription).catch(errorHandler);
+        // }
     }
 
     componentDidMount()
